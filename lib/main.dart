8919d3a2aab45e3cb3cff33eef89e9a0cf2688a8
mohamedabdelhamed555 <UGS.141478@ci.suez.edu.eg>
2024-08-16 20:58:16 +0300

    dark theme and create note view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note/views/note_views.dart';
 
 void main() {
   runApp(const NotesApp());
@@ -10,8 +11,9 @@ class NotesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
-      home: Scaffold(),
+      home: const NoteViews(),
     );
   }
 }
---
+++ lib/views/note_views.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NoteViews extends StatelessWidget {
+  const NoteViews({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
