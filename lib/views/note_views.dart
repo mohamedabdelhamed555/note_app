052815d52db036016210008c0462bafd2dcdf751
mohamedabdelhamed555 <UGS.141478@ci.suez.edu.eg>
2024-08-18 05:02:40 +0300

    create appbar and note item

--- lib/views/note_views.dart
+++ lib/views/note_views.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:note/views/widgets/notes_view_body.dart';
 
 class NoteViews extends StatelessWidget {
   const NoteViews({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      body: NotesViewBody(),
+    );
   }
 }
